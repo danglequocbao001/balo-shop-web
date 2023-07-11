2a5f6e99d6098dc31c36ce525c7451e18d079c80
Bao Dang <danglequocbao001@gmail.com>
2023-07-11 23:18:41 +0700

    chore: refactor products

--- src/components/products/Products.jsx
+++ src/components/products/Products.jsx
@@ -1,57 +1,19 @@
 import React from "react";
 import "react-loading-skeleton/dist/skeleton.css";
-import {
-  useFetchAllProducts,
-  useFetchAllNewProducts,
-  useFetchAllPromotionProducts,
-  useFetchAllBestSellerProducts,
-} from "../../hooks/useProducts";
+import { useFetchAllProducts, PRODUCTS_FILTER } from "../../hooks/useProducts";
 
 import ListProducts from "./ListProducts";
 
 const Products = () => {
-  const { products, isLoading: isProductsLoading } = useFetchAllProducts();
-  const { newProducts, isLoading: isNewProductsLoading } =
-    useFetchAllNewProducts();
-  const { promoteProducts, isLoading: isPromoteProductsLoading } =
-    useFetchAllPromotionProducts();
-  const { bestSellerProducts, isLoading: isBestSellerProductsLoading } =
-    useFetchAllBestSellerProducts();
-
-  {
-    /* <div className="mb-5 pb-5">
-          <button
-            className="btn btn-outline-dark me-3"
-            onClick={() => setFilter(data)}
-          >
-            All
-          </button>
-          <button
-            className="btn btn-outline-dark me-3"
-            onClick={() => filterProduct("men's clothing")}
-          >
-            Men's Clothing
-          </button>
-          <button
-            className="btn btn-outline-dark me-3"
-            onClick={() => filterProduct("women's clothing")}
-          >
-            Woman's Clothing
-          </button>
-          <button
-            className="btn btn-outline-dark me-3"
-            onClick={() => filterProduct("jewelery")}
-          >
-            Jewelery{" "}
-          </button>
-          <button
-            className="btn btn-outline-dark me-3"
-            onClick={() => filterProduct("electronics")}
-          >
-            Electronic
-          </button>
-        </div> */
-  }
+  // const { products, isLoading: isProductsLoading } = useFetchAllProducts();
+  const { products: newProducts, isLoading: isNewProductsLoading } =
+    useFetchAllProducts(PRODUCTS_FILTER.NEW);
+  const { products: promoteProducts, isLoading: isPromoteProductsLoading } =
+    useFetchAllProducts(PRODUCTS_FILTER.PROMOTE);
+  const {
+    products: bestSellerProducts,
+    isLoading: isBestSellerProductsLoading,
+  } = useFetchAllProducts(PRODUCTS_FILTER.BEST_SELLER);
 
   return (
     <div>
--- src/hooks/useProducts.js
+++ src/hooks/useProducts.js
@@ -2,7 +2,14 @@ import { useEffect, useState } from "react";
 import { toast } from "react-toastify";
 import { fetchAllProducts, fetchProductById } from "../services/products";
 
-export const useFetchAllProducts = () => {
+export const PRODUCTS_FILTER = {
+  ALL: "ALL",
+  NEW: "NEW",
+  PROMOTE: "PROMOTE",
+  BEST_SELLER: "BEST_SELLER",
+};
+
+export const useFetchAllProducts = (filter) => {
   const [isLoading, setLoading] = useState(false);
   const [data, setData] = useState([]);
 
@@ -10,85 +17,33 @@ export const useFetchAllProducts = () => {
     setLoading(true);
     fetchAllProducts()
       .then((data) => {
-        setData(data);
+        if (filter === PRODUCTS_FILTER.NEW) {
+          const result = data.filter((product) => {
+            return product.is_new === true;
+          });
+          setData(result);
+        } else if (filter === PRODUCTS_FILTER.PROMOTE) {
+          const result = data.filter((product) => {
+            return product.khuyen_mai !== undefined;
+          });
+          setData(result);
+        } else if (filter === PRODUCTS_FILTER.BEST_SELLER) {
+          const result = data.filter((product) => {
+            return product.chi_tiet_da_ban !== undefined;
+          });
+          setData(result);
+        } else {
+          setData(data);
+        }
         setLoading(false);
       })
       .catch((err) => {
         toast.error(err.message);
         setLoading(false);
       });
-  }, []);
+  }, [filter]);
 
   return { products: data, isLoading };
-};
-
-export const useFetchAllNewProducts = () => {
-  const [isLoading, setLoading] = useState(false);
-  const [data, setData] = useState([]);
-
-  useEffect(() => {
-    setLoading(true);
-    fetchAllProducts()
-      .then((data) => {
-        const result = data.filter((product) => {
-          return product.is_new === true;
-        });
-        setData(result);
-        setLoading(false);
-      })
-      .catch((err) => {
-        toast.error(err.message);
-        setLoading(false);
-      });
-  }, []);
-
-  return { newProducts: data, isLoading };
-};
-
-export const useFetchAllPromotionProducts = () => {
-  const [isLoading, setLoading] = useState(false);
-  const [data, setData] = useState([]);
-
-  useEffect(() => {
-    setLoading(true);
-    fetchAllProducts()
-      .then((data) => {
-        const result = data.filter((product) => {
-          return product.khuyen_mai !== undefined;
-        });
-        setData(result);
-        setLoading(false);
-      })
-      .catch((err) => {
-        toast.error(err.message);
-        setLoading(false);
-      });
-  }, []);
-
-  return { promoteProducts: data, isLoading };
-};
-
-export const useFetchAllBestSellerProducts = () => {
-  const [isLoading, setLoading] = useState(false);
-  const [data, setData] = useState([]);
-
-  useEffect(() => {
-    setLoading(true);
-    fetchAllProducts()
-      .then((data) => {
-        const result = data.filter((product) => {
-          return product.chi_tiet_da_ban !== undefined;
-        });
-        setData(result);
-        setLoading(false);
-      })
-      .catch((err) => {
-        toast.error(err.message);
-        setLoading(false);
-      });
-  }, []);
-
-  return { bestSellerProducts: data, isLoading };
 };
 
 export const useFetchOneProducts = (ma_mh) => {
