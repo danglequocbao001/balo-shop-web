c811eddb913afffe1c42e7501921074b390ae2c3
Bao Dang <danglequocbao001@gmail.com>
2023-07-20 22:43:49 +0700

    feat: show status

--- src/components/orders/Orders.jsx
+++ src/components/orders/Orders.jsx
@@ -1,8 +1,58 @@
-import React from "react";
+import React, { useState } from "react";
 import { useFetchAllOrder } from "../../hooks/useOrders";
 import OrderItem from "./OrderItem";
+import COLOR_CONSTANTS from "../../constants/colors";
+import { faGreaterThan } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+
 const Orders = () => {
   const { orders } = useFetchAllOrder();
+  const [isShowStatus, setIsShowStatus] = useState(false);
+
+  const status = (name, color, isHideGreater) => {
+    return (
+      <>
+        <p
+          style={{
+            backgroundColor: color,
+            padding: 5,
+            color: COLOR_CONSTANTS.WHITE,
+            fontWeight: "bold",
+            width: 180,
+            textAlign: "center",
+            borderRadius: 10,
+            marginRight: 10,
+          }}
+        >
+          {name}
+        </p>
+        {isHideGreater !== true && (
+          <FontAwesomeIcon
+            style={{
+              marginTop: 8,
+              marginRight: 10,
+            }}
+            icon={faGreaterThan}
+          />
+        )}
+      </>
+    );
+  };
+
+  const statusSteps = () => {
+    return (
+      <div
+        style={{
+          display: "flex",
+        }}
+      >
+        {status("Chờ thanh toán", COLOR_CONSTANTS.DARK_YELLOW)}
+        {status("Chờ duyệt", COLOR_CONSTANTS.DARK_YELLOW)}
+        {status("Chờ giao hàng", COLOR_CONSTANTS.DARK_YELLOW)}
+        {status("Đã hoàn thành", COLOR_CONSTANTS.SUCCESS, true)}
+      </div>
+    );
+  };
   return (
     <div
       style={{
@@ -17,8 +67,23 @@ const Orders = () => {
         *Bạn cần thanh toán trước khi nhân viên có thể duyệt đơn đặt hàng của
         bạn
       </h5>
+      <h5
+        style={{
+          color: "#8d8d8d",
+          textDecoration: "underline",
+          cursor: "pointer",
+        }}
+        onClick={() => setIsShowStatus(!isShowStatus)}
+      >
+        Chi tiết
+      </h5>
+      {isShowStatus && statusSteps()}
       {orders && (
-        <div>
+        <div
+          style={{
+            marginTop: 30,
+          }}
+        >
           {orders.map((order) => {
             return <OrderItem order={order} />;
           })}
